e3703de405081cfc1de1cc1c3e6682a78abcb676
aemann2 <aemann2@gmail.com>
2021-12-26 07:14:09 -0800

    began adding route config

--- server.ts
+++ server.ts
@@ -1,12 +1,33 @@
 import { Request, Response } from 'express';
+
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 dotenv.config({ path: './config/.env' });
 const db = require('./config/db');
 
 const app = express();
 const PORT: String | Number = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === 'production') {
+	// setting the static folder for the deployment
+	app.use(express.static('client/build'));
+	app.get('*', (req: Request, res: Response) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+}
+
+// body parser middleware
+app.use(express.json());
+app.use(
+	express.urlencoded({
+		extended: true,
+	})
+);
+
+// app.use('/scores', contacts);
+
+// Connecting to DB
 db();
 
 app.get('/api', (req: Request, res: Response) => {
