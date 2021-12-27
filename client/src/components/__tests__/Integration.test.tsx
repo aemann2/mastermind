babf360ee79195b544a8996423d7750e934ae9f8
aemann2 <aemann2@gmail.com>
2021-12-26 18:19:32 -0800

    wrote integration tests for Numbers, Modal, Guesses, and GuessHistory

--- client/src/components/__tests__/Integration.test.tsx
+++ client/src/components/__tests__/Integration.test.tsx
@@ -1,12 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-// import Guesses from '../Guesses';
-// import GuessHistory from '../GuessHistory';
-// import Numbers from '../Numbers';
 import App from '../../App';
-
-const setGuessNumber = jest.fn();
-const setGuessSequence = jest.fn();
 
 describe('Integration tests for Numbers and Guesses', () => {
 	test('Guesses renders correct number after submit button is clicked', () => {
@@ -19,7 +13,47 @@ describe('Integration tests for Numbers and Guesses', () => {
 	});
 });
 
-// a guess displays a modal with a message
-// a correct guess displays a modal with a message
-// a user's final guess displays a modal with a message
-// closing the final guess starts guess count over
+describe('Integration tests for Numbers and Modal', () => {
+	test('A correct guess displays a modal with a message', () => {
+		render(<App />);
+		const submitButton = screen.getByRole('button', {
+			name: 'Submit',
+		});
+		userEvent.click(submitButton);
+		const modal = screen.getByRole('dialog');
+		expect(modal).toBeInTheDocument();
+		expect(screen.getByText('Correct!')).toBeInTheDocument();
+	});
+	test("A user's final guess displays a modal with a message", () => {
+		render(<App />);
+		const submitButton = screen.getByRole('button', {
+			name: 'Submit',
+		});
+		for (let i = 0; i < 10; i++) {
+			userEvent.click(submitButton);
+		}
+		const modal = screen.getByRole('dialog');
+		expect(modal).toBeInTheDocument();
+		expect(screen.getByText('Nice try!')).toBeInTheDocument();
+	});
+});
+
+describe('Integration tests for Guesses, GuessHistory, and Modal', () => {
+	test('Closing gameloop modal resets Guess and GuessHistory', () => {
+		render(<App />);
+		const submitButton = screen.getByRole('button', {
+			name: 'Submit',
+		});
+		for (let i = 0; i < 10; i++) {
+			userEvent.click(submitButton);
+		}
+		const modal = screen.getByRole('dialog');
+		const closeButton = screen.getByRole('button', {
+			name: 'Close',
+		});
+		userEvent.click(closeButton);
+		expect(modal).not.toBeInTheDocument();
+		expect(screen.getByText('0 / 10')).toBeInTheDocument();
+		expect(screen.queryByText('Previous Guesses')).not.toBeInTheDocument();
+	});
+});
