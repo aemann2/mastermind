78524e098e1ba93d5fcb380d7e25870c41cd287b
aemann2 <aemann2@gmail.com>
2022-01-01 17:51:48 -0800

    cleaned up tests for InstructionsModal

--- client/src/components/__tests__/InstructionsModal.test.tsx
+++ client/src/components/__tests__/InstructionsModal.test.tsx
@@ -8,40 +8,28 @@ const closeModal = () => {
 	open = false;
 };
 
+const modalComponent = (
+	<InstructionsModal
+		instructionModalOpen={open}
+		setInstructionModalOpen={closeModal}
+	>
+		Test
+	</InstructionsModal>
+);
+
 describe('Tests for GameEndModal', () => {
 	test('Modal displays correct text', () => {
-		render(
-			<InstructionsModal
-				instructionModalOpen={open}
-				setInstructionModalOpen={closeModal}
-			>
-				Test
-			</InstructionsModal>
-		);
+		render(modalComponent);
 		const modal = screen.getByText('Test');
 		expect(modal).toBeInTheDocument();
 	});
 	test('Button displays for modal', () => {
-		render(
-			<InstructionsModal
-				instructionModalOpen={open}
-				setInstructionModalOpen={closeModal}
-			>
-				Test
-			</InstructionsModal>
-		);
+		render(modalComponent);
 		const button = screen.getByText('Close');
 		expect(button).toBeInTheDocument();
 	});
 	test('Button toggles modal open state', () => {
-		render(
-			<InstructionsModal
-				instructionModalOpen={open}
-				setInstructionModalOpen={closeModal}
-			>
-				Test
-			</InstructionsModal>
-		);
+		render(modalComponent);
 
 		const button = screen.getByText('Close');
 		userEvent.click(button);
