4b5fb074178b1b1940d8f88652c04dbd90a5bf46
aemann2 <aemann2@gmail.com>
2022-01-01 17:56:12 -0800

    cleaned up tests for GameEndModal.test

--- client/src/components/__tests__/GameEndModal.test.tsx
+++ client/src/components/__tests__/GameEndModal.test.tsx
@@ -8,32 +8,25 @@ const resetGame = () => {
 	open = false;
 };
 
+const gameEndModalComponent = (
+	<GameEndModal gameEndModalOpen={open} resetGame={resetGame}>
+		Test
+	</GameEndModal>
+);
+
 describe('Tests for GameEndModal', () => {
 	test('Modal displays correct text', () => {
-		render(
-			<GameEndModal gameEndModalOpen={open} resetGame={resetGame}>
-				Test
-			</GameEndModal>
-		);
+		render(gameEndModalComponent);
 		const modal = screen.getByText('Test');
 		expect(modal).toBeInTheDocument();
 	});
 	test('Button displays for modal', () => {
-		render(
-			<GameEndModal gameEndModalOpen={open} resetGame={resetGame}>
-				Test
-			</GameEndModal>
-		);
+		render(gameEndModalComponent);
 		const button = screen.getByText('Close');
 		expect(button).toBeInTheDocument();
 	});
 	test('Button toggles modal open state', () => {
-		render(
-			<GameEndModal gameEndModalOpen={open} resetGame={resetGame}>
-				Test
-			</GameEndModal>
-		);
-
+		render(gameEndModalComponent);
 		const button = screen.getByText('Close');
 		userEvent.click(button);
 		expect(open).toBe(false);
