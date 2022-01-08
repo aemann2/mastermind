59f8985c8d476cd35516ca16108de2960614e050
aemann2 <aemann2@gmail.com>
2022-01-07 17:22:19 -0800

    fetched scores from database for Scores page

--- client/src/pages/Scores/Scores.tsx
+++ client/src/pages/Scores/Scores.tsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { setAuthToken } from '../../utils/utils';
+
+interface Score {
+	_id: string;
+	sequence: string;
+	guesses: number;
+	solved: boolean;
+}
 
 const Scores = () => {
-	return <div>Scores</div>;
+	const [scores, setScores] = useState<Score[] | []>([]);
+
+	if (localStorage.token) {
+		setAuthToken(localStorage.token);
+	}
+
+	useEffect(() => {
+		getScores();
+	}, []);
+
+	async function getScores() {
+		const res = await axios.get(
+			'https://mastermind-amann.herokuapp.com/api/scores'
+		);
+		setScores(res.data.data);
+		console.log(res.data.data);
+	}
+
+	return (
+		<div>
+			Scores:
+			{scores.map((score) => (
+				<div key={score._id}>
+					<p>{score.sequence}</p>
+				</div>
+			))}
+		</div>
+	);
 };
 
 export default Scores;
