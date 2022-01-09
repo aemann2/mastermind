30732176635a5868bda12115cff34243d7e9ff2b
aemann2 <aemann2@gmail.com>
2022-01-09 13:01:01 -0800

    added unit tests for auth and signup routes

---
+++ tests/signup.test.ts
@@ -0,0 +1,32 @@
+export {};
+let server = require('../server.ts');
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+chai.should();
+chai.use(chaiHttp);
+
+describe('/post auth', () => {
+	it('Should return a 400 error if email is not submitted', (done) => {
+		chai
+			.request(server)
+			.post('/api/signup')
+			.set('content-type', 'application/json')
+			.send({ email: 'test', password: '123123' })
+			.end((err: any, res: any) => {
+				res.should.have.status(400);
+				done();
+			});
+	});
+	it('Should return a 400 error if password is too short', (done) => {
+		chai
+			.request(server)
+			.post('/api/signup')
+			.set('content-type', 'application/json')
+			.send({ email: 'test@test.com', password: '12312' })
+			.end((err: any, res: any) => {
+				res.should.have.status(400);
+				done();
+			});
+	});
+});
