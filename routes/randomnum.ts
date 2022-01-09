0f31134c9853f7e45ce523358685929a2376ce72
aemann2 <aemann2@gmail.com>
2022-01-09 10:27:08 -0800

    tweaked randomnum route to accommodate requests for between 4 - 6 numbers

--- routes/randomnum.ts
+++ routes/randomnum.ts
@@ -4,19 +4,34 @@ import { Request, Response } from 'express';
 const express = require('express');
 const router = express.Router();
 
+interface CustomQuery extends Request {
+	query: {
+		len: string;
+	};
+}
+
 // @route			GET api/randomnum
 // @desc			Get random number
 // @access		Public
-router.get('/', async (req: Request, res: Response) => {
-	function getRandomInt() {
-		const min = Math.ceil(0);
-		const max = Math.floor(8);
-		return Math.floor(Math.random() * (max - min) + min);
+router.get('/', async (req: CustomQuery, res: Response) => {
+	function getRandomInt(min: number, max: number) {
+		return Math.floor(Math.random() * (Math.ceil(max) - Math.ceil(min)) + min);
+	}
+
+	// checking if param exists. if not, set default number length to '4'
+	let { len } = req.query;
+	if (!len) len = '4';
+	const lenInt = parseInt(len);
+
+	if (lenInt < 4 || lenInt > 6 || isNaN(lenInt)) {
+		return res.status(400).json({
+			message: 'len param must be a number between 4 and 6',
+		});
 	}
 
 	let numArr = [];
-	for (let i = 0; i < 4; i++) {
-		const randomNum = getRandomInt();
+	for (let i = 0; i < lenInt; i++) {
+		const randomNum = getRandomInt(0, 8);
 		numArr.push(randomNum);
 	}
 
